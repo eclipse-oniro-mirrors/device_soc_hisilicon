91fc74e3c9e97a324af984e2bd409342040c3c41
suxiansheng32 <su_zebing@hoperun.com>
2026-05-12 03:11:58 +0000

    fix nearlink_dk_3863_xts build error
    Signed-off-by: suxiansheng32 <su_zebing@hoperun.com>

--- ws63v100/sdk/libs_url/ws63/cmake/ohos.cmake
+++ ws63v100/sdk/libs_url/ws63/cmake/ohos.cmake
@@ -4,7 +4,7 @@
 #===============================================================================
 
 if(${TARGET_COMMAND} MATCHES "ws63-liteos-xts")
-    set(COMPONENT_LIST "begetutil" "devattest_sdk" "hal_token_static" "hilog_lite_static" "huks_test_common" "module_ActsBootstrapTest" "module_ActsSamgrTest" "samgr_adapter" "bootstrap" "fsmanager_static" "hal_update_static" "hilog_static" "inithook" "module_ActsDeviceAttestTest" "module_ActsUpdaterFuncTest" "samgr_source"
+    set(COMPONENT_LIST "begetutil" "devattest_sdk" "hal_token_static" "hilog_lite_static" "huks_test_common" "module_ActsBootstrapTest" "module_ActsSamgrTest" "samgr_adapter" "bootstrap"  "hal_update_static" "hilog_static" "inithook" "module_ActsDeviceAttestTest" "module_ActsUpdaterFuncTest" "samgr_source"
             "broadcast" "hal_file_static" "hctest" "hiview_lite_static" "init_log" "module_ActsDfxFuncTest" "native_file" "udidcomm"
             "cjson_static" "hal_sys_param" "hichainsdk" "hota" "init_utils" "module_ActsHieventLiteTest" "param_client_lite"
             "devattest_core" "hal_sysparam" "hievent_lite_static" "huks_3.0_sdk"  "module_ActsHuksHalFunctionTest" "samgr"
@@ -12,7 +12,7 @@ if(${TARGET_COMMAND} MATCHES "ws63-liteos-xts")
 elseif(${TARGET_COMMAND} MATCHES "ws63-liteos-app")
 set(COMPONENT_LIST
 
-        "begetutil"   "hilog_lite_static" "samgr_adapter" "bootstrap" "fsmanager_static" "hal_update_static" "hilog_static" "inithook"   "samgr_source"
+        "begetutil"   "hilog_lite_static" "samgr_adapter" "bootstrap" "hal_update_static" "hilog_static" "inithook"   "samgr_source"
         "broadcast" "hal_file_static"   "init_log"  "native_file" "udidcomm"
         "cjson_static" "hal_sys_param" "hichainsdk" "hota" "init_utils"  "param_client_lite"
         "hiview_lite_static" "hal_sysparam" "hievent_lite_static" "huks_3.0_sdk"   "samgr" "blackbox_lite" "hal_iothardware" "wifiservice"
